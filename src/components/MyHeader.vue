61fdc6b8929f90811116665cd6eb44552d4ab7a2
Naoki Haba <naosprintrunner6385@gmail.com>
2024-04-23 01:02:04 +0900

    "MyButton", "MyHeader", "MyPage" コンポーネントを追加

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+
+import MyButton from "./MyButton.vue"
+
+const props = defineProps({
+  isLoggedIn: {
+    type: Boolean,
+    required: true
+  }
+})
+
+const emits = defineEmits([ 'login', 'logout', 'signup' ])
+
+</script>
+
+<template>
+  <header>
+    <h1>Storybook 7 + Vue 3 + TypeScript サンプル</h1>
+
+    <div class="button">
+      <template v-if="props.isLoggedIn">
+        <MyButton @click="$emit('logout')" label="ログアウト" size="small"/>
+      </template>
+      <template v-else>
+        <MyButton @click="$emit('login')" label="ログイン" size="small"/>
+        <MyButton @click="$emit('signup')" label="新規登録" size="small"/>
+      </template>
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+<
+style scoped >
+header {
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 15px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 45px;
+  background-color: #fff;
+}
+
+h1 {
+  font-weight: 900;
+  font-size: 20px;
+  line-height: 1;
+  margin: 6px 0 6px 10px;
+  display: inline-block;
+  vertical-align: top;
+}
+
+button + button {
+  margin-left: 10px;
+}
+
+@media screen and (max-width: 720px) {
+  .buttons {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/MyPage.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+
+import { ref } from "vue"
+import MyHeader from "./MyHeader.vue"
+
+type User = {
+  id: number;
+}
+
+const user = ref<User | null>(null)
+
+function login() {
+  user.value = {id: 1}
+}
+
+function logout() {
+  user.value = null
+}
+
+function signup() {
+  alert("新規登録")
+}
+
+</script>
+
+<template>
+
+  <div>
+    <MyHeader :is-logged-in="!!user" @login="login" @logout="logout" @signup="signup"/>
+    <main>
+      <div class="content-wrapper">
+        <div class="content" :key="i" v-for="i in 20">
+          {{ "コンテンツがここに表示されます".repeat(20) }}
+        </div>
+      </div>
+    </main>
+  </div>
+
+</template>
+
+<style>
+.content-wrapper {
+  background-color: #fff;
+  padding: 25px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 25px;
+  row-gap: 1em;
+}
+@media screen and (max-width: 720px) {
+  .content-wrapper {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+@media screen and (min-width: 721px) and (max-width: 1024px) {
+  .content-wrapper {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+</style>
