77e33492e9ca3ee6eebeb66d3be28b8668a47d28
Naoki Haba <naosprintrunner6385@gmail.com>
2024-04-26 00:15:23 +0900

    "MyButtonストーリーに新しいコンポーネントの定義を追加"

--- src/stories/MyButton.stories.ts
+++ src/stories/MyButton.stories.ts
@@ -1,18 +1,43 @@
-import { Meta, StoryObj } from "@storybook/vue3"
+import {Meta, StoryObj} from "@storybook/vue3"
 import MyButton from "../components/MyButton.vue"
 
+// コンポーネントのプロパティと対応するストーリーの設定を定義するためのおまじない
 type Story = StoryObj<typeof MyButton>
 
+// MyButtonコンポーネントに対するメタ情報を定義
 const meta: Meta<typeof MyButton> = {
     title: "MyButton",
-    component: MyButton
+    component: MyButton,
+
+    // レンダリング方法を定義
+    render: (args) => ({
+        components: {MyButton},
+
+        // Composition APIのsetup関数を使って、propsを定義
+        setup() {
+            return {args}
+        },
+        // args: propsを定義し、Storybookの画面上で動的に変更できるようにする機能
+        template: '<MyButton v-bind="args" />'
+    })
 }
 
-export const Default: Story = ( {
-    render: () => ( {
-        components: {MyButton},
-        template: '<my-button label="ボタン"/>'
-    } )
-} )
+export const Default: Story = {
+    args: {
+        label: "ボタン"
+    }
+}
+
+export const Login: Story = {
+    args: {
+        label: "ログイン"
+    }
+}
+
+export const SignUp: Story = {
+    args: {
+        label: "会員登録"
+    }
+}
 
 export default meta
